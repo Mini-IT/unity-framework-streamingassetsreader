c5b1addd7252a082d1526b9adc103fc0f95f22cf
gluks <gluk-02@yandex.ru>
2024-01-15 19:41:22 +0600

    Update DefaultStreamingAssetsReader.cs

--- Runtime/DefaultStreamingAssetsReader.cs
+++ Runtime/DefaultStreamingAssetsReader.cs
@@ -9,39 +9,32 @@ namespace MiniIT.Unity
 	{
 		public async UniTask<string> ReadTextAsync(string path, CancellationToken cancellationToken = default)
 		{
-			if (File.Exists(path))
+			if (!File.Exists(path))
 			{
-				try
-				{
-#if UNITY_2021_1_OR_NEWER
-					return await File.ReadAllTextAsync(path, cancellationToken);
-#else
-					return await UniTask.FromResult(File.ReadAllText(path))
-						.AttachExternalCancellation(cancellationToken);
-#endif
-				}
-				catch (OperationCanceledException)
-				{
-					// Suppress cancelation throw
-				}
+				return null;
 			}
 
-			return null;
+#if UNITY_2021_1_OR_NEWER
+			return await File.ReadAllTextAsync(path, cancellationToken);
+#else
+			return await UniTask.FromResult(File.ReadAllText(path))
+				.AttachExternalCancellation(cancellationToken);
+#endif
 		}
 
 		public async UniTask<byte[]> ReadBytesAsync(string path, CancellationToken cancellationToken = default)
 		{
-			if (File.Exists(path))
+			if (!File.Exists(path))
 			{
-#if UNITY_2021_1_OR_NEWER
-				return await File.ReadAllBytesAsync(path, cancellationToken);
-#else
-				return await UniTask.FromResult(File.ReadAllBytes(path))
-					.AttachExternalCancellation(cancellationToken);
-#endif
+				return null;
 			}
 
-			return null;
+#if UNITY_2021_1_OR_NEWER
+			return await File.ReadAllBytesAsync(path, cancellationToken);
+#else
+			return await UniTask.FromResult(File.ReadAllBytes(path))
+				.AttachExternalCancellation(cancellationToken);
+#endif
 		}
 	}
 }
